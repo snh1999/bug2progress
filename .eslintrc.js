a227cbf5c267c38d2a907a784adf95d923f33071
Sahriar Nahin <sahriar.nahin@sazim.io>
2024-09-21 23:31:16 +0600

    feat: updates configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,65 @@
 module.exports = {
+  env: {
+    jest: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        jest: true,
+      },
+      files: ['**/*.spec.ts', '**/*.test.ts'],
+    },
+  ],
+  ignorePatterns: ['.eslintrc'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  plugins: ['@typescript-eslint/eslint-plugin', 'eslint-plugin-import-helpers'],
   root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    'prettier/prettier': [
-      'error',
+    'arrow-body-style': ['warn', 'as-needed'],
+    eqeqeq: ['warn', 'always'],
+    'linebreak-style': ['warn', 'unix'],
+    'eol-last': ['warn', 'always'],
+    'no-restricted-imports': [
+      'warn',
       {
-        endOfLine: 'auto',
+        patterns: ['../../../*', '!../*', '!../../*', '!test/**/*'],
       },
     ],
+    'no-const-assign': 'warn',
+    'no-lone-blocks': 'warn',
+    'no-return-await': 'warn',
+    'no-trailing-spaces': 'warn',
+    'no-throw-literal': 'warn',
+    'no-unused-expressions': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+    'no-useless-catch': 'warn',
+    'no-useless-escape': 'warn',
+    'no-useless-return': 'warn',
+    'no-unneeded-ternary': 'warn',
+    'no-unreachable': 'warn',
+    'no-var': 'warn',
+    'prefer-const': 'warn',
+    quotes: ['off', 'double'],
+    'require-await': 'warn',
+    semi: ['warn', 'always'],
+    'spaced-comment': ['warn', 'always', { exceptions: ['-', '+'] }],
   },
 };
--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,57 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import ratelimit from 'express-rate-limit';
 import helmet from 'helmet';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+} from '@nestjs/swagger';
+import { getAllowedMethods, getCorsConfig } from './common/config/cors.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+    prefix: 'api/v',
+  });
+  app.enableCors({
+    methods: getAllowedMethods(),
+    ...getCorsConfig(),
+  });
+
   app.use(helmet());
   app.use(cookieParser());
+
   app.use(
     ratelimit({
-      max: parseInt(process.env.MAX_REQ_HR),
+      max: parseInt(process.env.MAX_REQ_HR || '100'),
       windowMs: 60 * 60 * 1000,
       message: 'Too many requests from your network, Please try again later',
     }),
   );
+  if (process.env.NODE_ENV === 'local') {
+    const config = new DocumentBuilder()
+      .setTitle('bug2progress')
+      .setDescription('bug2progress API description')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
 
-  const config = new DocumentBuilder()
-    .setTitle('bug2progress')
-    .setDescription('bug2progress API description')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-  app.setGlobalPrefix('api/v1');
+    const documentOptions: SwaggerDocumentOptions = {
+      ignoreGlobalPrefix: true,
+      operationIdFactory: (_: string, methodKey: string) => methodKey,
+    };
+
+    const document = SwaggerModule.createDocument(app, config, documentOptions);
+    SwaggerModule.setup('api', app, document);
+  }
+
   await app.listen(8080);
 }
+
 bootstrap();
